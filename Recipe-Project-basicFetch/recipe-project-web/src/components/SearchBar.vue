6cd3f6fa31150d69085f6f8d9b0e012c660a0ba5
LOUISE ERICSSON <louise.ericsson4@gmail.com>
2022-10-18 12:01:22 +0200

    Update searchbar

--- Recipe-Project-basicFetch/recipe-project-web/src/components/SearchBar.vue
+++ Recipe-Project-basicFetch/recipe-project-web/src/components/SearchBar.vue
@@ -6,22 +6,9 @@ export default {
             searchBarInput: null,
         }
     },
-    computed: {
+    methods: {
         filteredListOfRecipes() {
-
-            let parsedRecipes = JSON.parse(JSON.stringify(this.searchBarListOfRecipes))
-            console.log(parsedRecipes);
-            let filteredRecipes = [];
-
-            for (let recipe in parsedRecipes) {
-   
-                console.log(recipe)
-                filteredRecipes.push(recipe)
-            }
-
-            console.log(filteredRecipes)
-
-            return filteredRecipes;
+            return this.searchBarListOfRecipes.filter(recipe => recipe.title.toLowerCase().includes(this.searchBarInput.toLowerCase()));
         }
     }
 }
@@ -29,10 +16,21 @@ export default {
 
 <template>
 
-    <input v-model="searchBarInput" placeholder="edit me" />
+    <input type="text" v-model="searchBarInput" placeholder="Search" />
+    <button type="button" @click="filteredListOfRecipes">Search</button>
 
-    <li v-for="recipe in filteredListOfRecipes" :key="recipe">
-        {{ recipe.title }}
-        {{ recipe.avgRating }}
-    </li>
+    <!-- <ul>
+        <li v-for="recipe in filteredListOfRecipes" :key="recipe">
+            {{ recipe.title }}
+            {{ recipe.description.substring(0, 50) + '...' }}
+            {{ recipe.avgRating }}
+        <li v-for="category in recipe.categories">
+            {{category}}
+        </li>
+        <img :src=recipe.imageUrl alt="recipeImage" width="200">
+        </li>
+    </ul> -->
+    <!-- <div class="item error" v-if="input&&!filteredList().length">
+        <p>No results found!</p>
+    </div> -->
 </template>
