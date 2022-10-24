24b2c4ae8cbd0e2e5418460f0096d2e815bc7480
LOUISE ERICSSON <louise.ericsson4@gmail.com>
2022-10-24 09:59:26 +0200

    Searchbar component updated

--- recipe-project-web/src/components/SearchBar.vue
+++ recipe-project-web/src/components/SearchBar.vue
@@ -1,36 +1,20 @@
 <script>
 export default {
-    props: ['searchBarListOfRecipes'],
+    emits: ["messageChanged"],
     data() {
         return {
-            searchBarInput: null,
+            message: '',
         }
     },
     methods: {
-        filteredListOfRecipes() {
-            return this.searchBarListOfRecipes.filter(recipe => recipe.title.toLowerCase().includes(this.searchBarInput.toLowerCase()));
+        changeMessage(event) {
+            this.message = event.target.value;
+            this.$emit('messageChanged', this.message);
         }
     }
 }
 </script>
 
 <template>
-
-    <input type="text" v-model="searchBarInput" placeholder="Search" />
-    <button type="button" @click="filteredListOfRecipes">Search</button>
-
-    <!-- <ul>
-        <li v-for="recipe in filteredListOfRecipes" :key="recipe">
-            {{ recipe.title }}
-            {{ recipe.description.substring(0, 50) + '...' }}
-            {{ recipe.avgRating }}
-        <li v-for="category in recipe.categories">
-            {{category}}
-        </li>
-        <img :src=recipe.imageUrl alt="recipeImage" width="200">
-        </li>
-    </ul> -->
-    <!-- <div class="item error" v-if="input&&!filteredList().length">
-        <p>No results found!</p>
-    </div> -->
+    <input type="text" @input="changeMessage" placeholder="Search"/>
 </template>
